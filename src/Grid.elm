8b5f51a7855c38e7c70c69e15d10b9eefeb8c7b2
David Mills <dave@1morepx.com>
2017-02-19 19:56:58 -0500

    Draw a grid

--- src/Grid.elm
+++ src/Grid.elm
@@ -1,10 +1,77 @@
 module Grid exposing (..)
 
 import Html exposing (Html, div, h2, text)
+import Collage exposing (traced, segment, solid, Form)
+import Element
+import Color
 
 
 view : Html msg
 view =
     div []
         [ h2 [] [ text "Grid" ]
+        , Collage.collage 400 400 [ drawGrid ]
+            |> Element.toHtml
         ]
+
+
+drawGrid : Form
+drawGrid =
+    let
+        width =
+            400.0
+
+        height =
+            400.0
+
+        gridSize =
+            25.0
+
+        rows =
+            width / gridSize
+
+        columns =
+            height / gridSize
+
+        rowSegements =
+            List.range 0 (floor rows)
+                |> List.map
+                    (\r ->
+                        let
+                            x =
+                                (toFloat r) * gridSize - height / 2
+
+                            start =
+                                ( x, -height / 2 )
+
+                            end =
+                                ( x, height / 2 )
+                        in
+                            segment start end
+                    )
+
+        columnSegments =
+            List.range 0 (floor columns)
+                |> List.map
+                    (\r ->
+                        let
+                            y =
+                                (toFloat r) * gridSize - width / 2
+
+                            start =
+                                ( -width / 2, y )
+
+                            end =
+                                ( width / 2, y )
+                        in
+                            segment start end
+                    )
+
+        gridSegments =
+            List.append rowSegements columnSegments
+
+        form =
+            List.map (\r -> traced (solid Color.black) r) gridSegments
+                |> Collage.group
+    in
+        form
