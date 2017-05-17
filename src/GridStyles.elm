63bf8ba60d119b3da7d97a9bb8dada21ec460af2
David Mills <dave@1morepx.com>
2017-05-17 16:24:26 -0400

    add hover styles in grid

--- src/GridStyles.elm
+++ src/GridStyles.elm
@@ -2,6 +2,7 @@ module GridStyles exposing (..)
 
 import Css exposing (..)
 import Css.Namespace exposing (namespace)
+import Css.Colors exposing (yellow)
 
 
 type Classes
@@ -17,5 +18,15 @@ css =
         , class Cell
             [ width (px 32)
             , height (px 32)
+            , hover
+                [ before
+                    [ property "content" "''"
+                    , position absolute
+                    , backgroundColor yellow
+                    , opacity (num 0.25)
+                    , width (px 32)
+                    , height (px 32)
+                    ]
+                ]
             ]
         ]
