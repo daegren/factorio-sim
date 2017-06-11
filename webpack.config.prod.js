dda9ac9c2876e79f32f55346cdb6fc4acc981d5e
David Mills <dave@1morepx.com>
2017-06-11 13:43:40 -0400

    update public path to make sense

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,7 @@ module.exports = {
   output : {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].js',
-    publicPath: '/build/',
+    publicPath: '/factorio-sim/build/',
   },
   resolve: {
     alias: {
