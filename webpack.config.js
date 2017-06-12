7545444b5f7731d5cd20cef6ef9aa391e4b394a4
David Mills <dave@1morepx.com>
2017-06-12 12:23:54 -0400

    fixing dev webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output : {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/factorio-sim/build/'
+    publicPath: '/build/'
   },
   resolve: {
     alias: {
